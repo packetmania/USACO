ce35cbb0760f8e43ac2d7a231b856dcb82a4cd6e
shuxiao <shuxiao@gmail.com>
2025-11-27 19:42:38 -0800

    Updated Y2024 Jan Bronze Problem 3 solution with a new optimized solve_fast() that has time complexity O(n).

--- Bronze/Y2024-Jan-Bronze-P3.py
+++ Bronze/Y2024-Jan-Bronze-P3.py
@@ -1,7 +1,7 @@
 # USACO 2024 January Contest, Bronze: P3. Balancing Bacteria
 
 def solve():
-    # Solution with simulation approach algorithm:
+    # Solution with simulation approach:
     #   For each a[i], if it is not 0, apply the operation to make it 0,
     #   and update all a[j] (j>i) accordingly. Count the number of operations.
     #
@@ -10,7 +10,7 @@ def solve():
     a = [int(x) for x in input().split()]
     num = 0
 
-    for i in range(0, N):
+    for i in range(N):
         if a[i] != 0:
             for j in range(i+1, N):
                 a[j] += -a[i] * (j - i + 1)
@@ -19,23 +19,30 @@ def solve():
     return num
 
 
-def solve():
-    # Solution with simulation approach algorithm:
-    #   For each a[i], if it is not 0, apply the operation to make it 0,
-    #   and update all a[j] (j>i) accordingly. Count the number of operations.
+def solve_fast():
+    # Solution with prefix-sum like algorithm:
     #
-    # Time complexity O(n^2)
+    # Time complexity O(n)
     N = int(input())
     a = [int(x) for x in input().split()]
-    num = 0
 
-    for i in range(0, N):
-        if a[i] != 0:
-            for j in range(i + 1, N):
-                a[j] += -a[i] * (j - i + 1)
-            num += abs(a[i])  ## Remember to use abs() here!
+    num = 0      # total number of operations
+    effects = 0  # total effect on current position from all previous operations
+    sum_ops = 0  # algebraic sum of operations (+1 for add, -1 for remove)
+
+    for i in range(N):
+        effects += sum_ops  # update effects from all previous operations
+        a[i] += effects     # apply effects to a[i]
+
+        cur_ops = -a[i]      # number of operations needed make a[i] become 0
+        num += abs(cur_ops)  # update total number of operations
+
+        sum_ops += cur_ops   # # update the global operation algebraic sum
+
+        # these new operations also affect the current position immediately
+        effects += cur_ops
 
     return num
 
 if __name__ == "__main__":
-    print(solve())
+    print(solve_fast())
